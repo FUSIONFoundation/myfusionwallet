0ad7f8a0aff2c0e94a4e5bff580c158ee0e96d57
Joey Kheireddine <joey.kheireddine@outlook.com>
2019-01-06 16:59:53 +0100

    Added Ledger support for TimeLock to TimeLock

--- app/scripts/controllers/sendTxCtrl.js
+++ app/scripts/controllers/sendTxCtrl.js
@@ -1014,7 +1014,9 @@ var sendTxCtrl = function ($scope, $sce, walletService, $rootScope) {
                 amount = new BN(amount + dec + "0".repeat(parseInt(declen)));
             }
 
-            if (!$scope.account) {
+            let data = {};
+
+            if (!$scope.account && ($scope.wallet.hwType !== "ledger")) {
                 $scope.account = web3.eth.accounts.privateKeyToAccount($scope.toHexString($scope.wallet.getPrivateKey()));
             }
 
@@ -1027,6 +1029,11 @@ var sendTxCtrl = function ($scope, $sce, walletService, $rootScope) {
                     end: tillTime,
                     value: amount
                 }).then((tx) => {
+                    tx.from = from;
+                    data = tx;
+                    if ($scope.wallet.hwType == "ledger") {
+                        return;
+                    }
                     return web3.fsn.signAndTransmit(tx, $scope.account.signTransaction).then(txHash => {
                         $scope.$eval(function () {
                             $scope.sendAssetFinal.open();
@@ -1038,6 +1045,60 @@ var sendTxCtrl = function ($scope, $sce, walletService, $rootScope) {
             } catch (err) {
                 $scope.errorModal.open();
             }
+
+            if ($scope.wallet.hwType == "ledger") {
+                let ledgerConfig = {
+                    privKey: $scope.wallet.privKey ? $scope.wallet.getPrivateKeyString() : "",
+                    path: $scope.wallet.getPath(),
+                    hwType: $scope.wallet.getHWType(),
+                    hwTransport: $scope.wallet.getHWTransport()
+                }
+                let rawTx = data;
+                var eTx = new ethUtil.Tx(rawTx);
+                if (ledgerConfig.hwType == "ledger") {
+                    var app = new ledgerEth(ledgerConfig.hwTransport);
+                    var EIP155Supported = true;
+                    var localCallback = async function (result, error) {
+                        if (typeof error != "undefined") {
+                            if (callback !== undefined) callback({
+                                isError: true,
+                                error: error
+                            });
+                            return;
+                        }
+                        var splitVersion = result['version'].split('.');
+                        if (parseInt(splitVersion[0]) > 1) {
+                            EIP155Supported = true;
+                        } else if (parseInt(splitVersion[1]) > 0) {
+                            EIP155Supported = true;
+                        } else if (parseInt(splitVersion[2]) > 2) {
+                            EIP155Supported = true;
+                        }
+                        var oldTx = Object.assign(rawTx, {});
+                        let input = oldTx.input;
+                        return uiFuncs.signed(app, rawTx, ledgerConfig, true, function (res) {
+                            oldTx.r = res.r;
+                            oldTx.s = res.s;
+                            oldTx.v = res.v;
+                            oldTx.input = input;
+                            oldTx.chainId = "0x1";
+                            delete oldTx.isError;
+                            delete oldTx.rawTx;
+                            delete oldTx.signedTx;
+                            web3.fsntx.sendRawTransaction(oldTx).then(function (txHash) {
+                                $scope.$eval(function () {
+                                    $scope.sendAssetFinal.open();
+                                    $scope.successHash = txHash;
+                                    $scope.successHash = txHash;
+                                });
+                            })
+                        })
+                    }
+                    $scope.notifier.info('Please, confirm transaction on Ledger.');
+                    await app.getAppConfiguration(localCallback);
+                }
+            }
+
         }
 
         $scope.createAssetInit = function () {
