8af300b2350373d836cfcc18e30015d4a3b1108f
Joey Kheireddine <joey.kheireddine@outlook.com>
2019-03-27 23:36:30 +0100

    Fixed issue on sorting on swap market

--- app/scripts/controllers/ensCtrl.js
+++ app/scripts/controllers/ensCtrl.js
@@ -240,6 +240,7 @@ var ensCtrl = function ($scope, $sce, walletService, $rootScope) {
         'swapId': '',
         'fromAssetId': ''
     };
+    $scope.sortKey = 'timePosix';
     $scope.sortSwapMarket = function (keyname) {
         $scope.sortKey = keyname;   //set the sortKey to the param passed
         $scope.reverse = !$scope.reverse; //if true make it false and vice versa
