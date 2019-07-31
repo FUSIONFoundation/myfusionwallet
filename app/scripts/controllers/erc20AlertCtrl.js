563edea18e5624ac5fb8ba8a13a5aa7d3fbb2576
Idriss Dimson <idriss.dimson@gmail.com>
2019-07-31 16:23:35 -0400

    added a 1170px container for banner

--- app/scripts/controllers/erc20AlertCtrl.js
+++ app/scripts/controllers/erc20AlertCtrl.js
@@ -5,8 +5,12 @@ var erc20AlertCtrl = function($scope) {
         let alertData = {"shown": true};
         localStorage.setItem("cookieName",JSON.stringify(alertData));
     } else {
-        //set the erc20Alert to the value of the localStorage variable (true)
-        $scope.erc20Alert = true;
+        if (localStorage.getItem("cookieName") === true){
+            //set the erc20Alert to the value of the localStorage variable (true)
+            $scope.erc20Alert = true;
+        } else {
+            $scope.erc20Alert = false;
+        }
     }
 }
 module.exports = erc20AlertCtrl;
